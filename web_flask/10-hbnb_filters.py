5356bdb8329b348eb606f1ecb9d98ff69e0b807e
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-22 19:02:00 +0100

    HBNB is alive!

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""
+Starts a Flask web application for displaying HBnB filters.
+
+This script starts a Flask web application that displays HBnB filters
+including States and Amenities.
+
+Web Application Routes:
+- '/hbnb_filters': Displays a webpage with HBnB filters.
+
+Usage:
+    - Run this script to start the Flask web application.
+    - Access the '/hbnb_filters' route in your web browser to see the filters.
+"""
+
 from models import storage
 from flask import Flask
 from flask import render_template
@@ -9,7 +22,12 @@ app = Flask(__name__)
 
 @app.route("/hbnb_filters", strict_slashes=False)
 def hbnb_filters():
-    """Displays the main HBnB filters HTML page."""
+    """
+    Route that renders an HTML page displaying HBnB filters.
+
+    Returns:
+        str: Rendered HTML page with HBnB filters (States and Amenities).
+    """
     states = storage.all("State")
     amenities = storage.all("Amenity")
     return render_template("10-hbnb_filters.html",
@@ -18,7 +36,12 @@ def hbnb_filters():
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """
+    Teardown method to remove the current SQLAlchemy Session.
+
+    Args:
+        exc: The exception, if any.
+    """
     storage.close()
 
 
--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,24 +1,52 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""
+Starts a Flask web application for displaying HBnB filters.
+
+This script starts a Flask web application that displays HBnB filters.
+It includes States and Amenities and uses the Flask framework to serve
+web pages.
+
+Web Application Routes:
+- '/hbnb': Displays a webpage with HBnB filters and places.
+
+Usage:
+    - Run this script to start the Flask web application.
+    - Access the '/hbnb' route in your web browser
+    - to see the filters and places.
+
+Author:
+    Your Name
+"""
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/hbnb_filters", strict_slashes=False)
 def hbnb_filters():
-    """Displays the main HBnB filters HTML page."""
+    """
+    Route that renders an HTML page displaying HBnB filters and places.
+
+    Returns:
+        str: Rendered HTML page with HBnB filters and places.
+    """
     states = storage.all("State")
     amenities = storage.all("Amenity")
+    places = storage.all("Place")
     return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
+                           states=states, amenities=amenities, places=places)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """
+    Teardown method to remove the current SQLAlchemy Session.
+
+    Args:
+        exc: The exception, if any.
+    """
     storage.close()
 
 
