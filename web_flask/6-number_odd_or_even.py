ff41aa222912cbe9c981efd466bc6ce0e1d17304
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-22 18:26:21 +0100

    Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,52 +1,125 @@
 #!/usr/bin/python3
-"""Flask"""
+"""
+Flask Web Application
+
+This script starts a Flask web application with various routes
+for different purposes.
+
+Routes:
+- '/': Returns a greeting message.
+- '/hbnb': Returns "HBNB".
+- '/c/<text>': Displays "C" followed by the value of the text variable.
+- '/python' and '/python/<text>': Displays "Python" followed by:
+    value - the text variable.
+- '/number/<int:n>': Displays "<n> is a number" if n is an integer.
+- '/number_template/<int:n>': Displays an HTML page with
+    the number if n is an integer.
+- '/number_odd_or_even/<int:n>': Displays an HTML page with information
+    about whether n is even or odd.
+
+Usage:
+    Run this script to start the Flask web application.
+    Access the defined routes in your web browser.
+"""
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Return a given string"""
+    """
+    Route that returns a greeting.
+    
+    Returns:
+        str: A greeting message.
+    """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns a given string"""
+    """
+    Route that returns "HBNB".
+    
+    Returns:
+        str: "HBNB"
+    """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cText(text):
-    """display C followed by the value of the text variable"""
+    """
+    Route that displays "C" followed by the value of the text variable.
+    
+    Args:
+        text (str): The text to be displayed.
+        
+    Returns:
+        str: "C" followed by the modified text.
+    """
     return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythonText(text="is cool"):
-    """display Python followed by the value of the text variable"""
+    """
+    Route that displays "Python" followed by the value of the text variable.
+    
+    Args:
+        text (str, optional): The text to be displayed. Defaults to "is cool".
+    
+    Returns:
+        str: "Python" followed by the modified text.
+    """
     return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def isNumber(n):
-    """display “n is a number” only if n is an integer"""
+    """
+    Route that displays "<n> is a number" if n is an integer.
+    
+    Args:
+        n (int): The number to be checked.
+        
+    Returns:
+        str: "<n> is a number" if n is an integer.
+    """
     if isinstance(n, int):
         return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n=None):
-    """display a HTML page only if n is an integer"""
+    """
+    Route that displays an HTML page with the number if n is an integer.
+    
+    Args:
+        n (int, optional): The number to be displayed in the HTML page.
+        Defaults to None.
+    
+    Returns:
+        str: HTML page with the number if n is an integer.
+    """
     if isinstance(n, int):
         return render_template("5-number.html", n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n=None):
-    """display a HTML page only if n is an integer:
-    H1 tag: “Number: n is even|odd” inside the tag BODY
+    """
+    Route that displays an HTML page with information
+    about whether n is even or odd.
+    
+    Args:
+        n (int, optional): The number to be checked. Defaults to None.
+    
+    Returns:
+        str: HTML page with information about whether n is even or odd.
     """
     if isinstance(n, int):
         if n % 2:
