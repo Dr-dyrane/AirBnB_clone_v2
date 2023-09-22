ded2408d51e53c4987a9ebbe4cd233c16b4d5e75
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-22 18:47:21 +0100

    States and State

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
+"""
+Web Application with Flask
+
+This script starts a Flask web application to display
+a list of States and their associated Cities.
+
+Web Application Routes:
+- '/cities_by_states': Displays a list of States with their associated Cities.
+
+Usage:
+    - Run this script to start the Flask web application.
+    - Access the '/cities_by_states' route in your web browser
+    - to see the list of States and Cities.
+"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -10,13 +24,21 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close(self):
-    """ Method to close the session """
+    """
+    Teardown method to remove the current SQLAlchemy Session.
+    """
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Displays a html page with states and cities"""
+    """
+    Route that renders an HTML page displaying
+    a list of States with their associated Cities.
+
+    Returns:
+        str: Rendered HTML page with States and Cities.
+    """
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
