7055945373f801f54d139f43e841059f70881fc2
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-22 17:53:31 +0100

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,10 +4,10 @@
 The web application listens on 0.0.0.0, port 5000, and defines four routes:
 - '/': Displays "Hello HBNB!" when accessed.
 - '/hbnb': Displays "HBNB" when accessed.
-- '/c/<text>': Displays "C " followed by the value of the text variable, 
+- '/c/<text>': Displays "C " followed by the value of the text variable,
   with underscores (_) replaced by spaces.
-- '/python/<text>': Displays "Python " followed by the value of the text variable,
-  with underscores (_) replaced by spaces. The default value of text is "is cool".
+- '/python/<text>': Displays "Python " followed by - value of - text variable,
+  with (_) replaced by spaces. The default value of text is "is cool".
 
 Usage:
     Run this script directly to start the Flask web application.
@@ -16,6 +16,7 @@ Usage:
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
@@ -26,6 +27,7 @@ def hello():
     """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Display a message.
@@ -34,6 +36,7 @@ def hbnb():
         str: A string containing the message "HBNB".
     """
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def cText(text):
@@ -47,6 +50,7 @@ def cText(text):
     """
     return "C {}".format(text.replace("_", " "))
 
+
 @app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def pythonText(text):
@@ -59,6 +63,7 @@ def pythonText(text):
         str: A string containing "Python " followed by the processed text.
     """
     return "Python {}".format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
